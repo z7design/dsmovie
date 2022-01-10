192915c121313d1753efbc3fe29e8f407d433257
Jose Mario Ramos <z7designcode@gmail.com>
2022-01-10 12:20:56 -0300

    Projeto Created

---
+++ backend/src/main/java/com/z7design/dsmove/DsmoveApplication.java
@@ -0,0 +1,13 @@
+package com.z7design.dsmove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmoveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/z7design/dsmove/DsmoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z7design.dsmove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
