6b680546c1fef6ff70004a49389ced79bc5f9ebf
Jose Mario Ramos <z7designcode@gmail.com>
2022-03-09 16:08:38 -0300

    Find movies

---
+++ backend/src/main/java/com/z7design/dsmove/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.z7design.dsmove.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.z7design.dsmove.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
