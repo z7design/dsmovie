8b476c472608b36ca3734a6f40111428072b56d0
Jose Mario Ramos <z7designcode@gmail.com>
2022-03-12 10:34:54 -0300

    useParams

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -4,11 +4,39 @@ import {ReactComponent as StarHalf} from 'assets/img/star-half.svg';
 import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 import './styles.css';
 
-function MovieStars() {
+type Props = {
+score: number;
+}
+
+// EX:
+// getFills(3.5) => [1, 1, 1, 0.5, 0]
+// getFills(4.1) => [1, 1, 1, 1, 0.5]
+function getFills(score: number) {
+
+    const fills = [0, 0, 0, 0, 0];
+  
+    const integerPart = Math.floor(score);
+  
+    for (let i = 0; i < integerPart; i++) {
+      fills[i] = 1;
+    }
+  
+    const diff = score - integerPart;
+    if (diff > 0) {
+      fills[integerPart] = 0.5;
+    }
+  
+    return fills;
+  }
+
+function MovieStars({ score } : Props) {
+
+const fills = getFills(score);
+
     return (
 
         <div className="dsmovie-stars-container">
-            <StarFull />
+            <StarFull fill={fills[0]}/>
             <StarFull />
             <StarFull />
             <StarHalf />
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
@@ -21,7 +22,7 @@ function Listing() {
         });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}%sort=id`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}%sort=title`)
     .then(response => {
         const data = response.data as MoviePage;
            setPage(data); 
