6806766059f189808a16c5f48e5dbafef76ea9bd
Jose Mario Ramos <z7designcode@gmail.com>
2022-03-12 11:56:50 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -6,6 +6,8 @@ import { useState, useEffect } from "react";
 import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
+
+
 function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
@@ -22,16 +24,21 @@ function Listing() {
         });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}%sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
     .then(response => {
         const data = response.data as MoviePage;
            setPage(data); 
         });
     }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
     
     return (
        <>
-        <Pagination />
+        <Pagination page={page} onChange={handlePageChange} />
+
             <div className="container">
                 <div className="row">
                    {page.content.map(movie => (
